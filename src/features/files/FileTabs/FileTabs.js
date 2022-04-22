e449327a3ea6f8c179516fda33fa20255d1fb8c2
Alexia Standish <alexiastandish@Alexias-MacBook-Pro.local>
2022-04-22 03:01:16 -0700

    for demo

--- src/features/files/FileTabs/FileTabs.js
+++ src/features/files/FileTabs/FileTabs.js
@@ -1,12 +1,17 @@
 import React, { useState } from 'react'
 import FileTabLabel from './FileTabLabel'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import './FileTabs.css'
+import { addNewBlock } from 'thunks/add-new-block'
+import { blocksSelector } from '../../blocks/blocksSelectors'
 
 function FileTabs({ children }) {
+    const dispatch = useDispatch()
     const files = useSelector((state) => state.files.files)
     const activeFile = useSelector((state) => state.files.activeFile)
     const findFile = files.findIndex((el) => el.id === activeFile)
+    const allBlocks = useSelector((state) => blocksSelector.selectAll(state))
+
     return (
         <div>
             <ul className="tabrow">
@@ -19,6 +24,9 @@ function FileTabs({ children }) {
                         selectedTab={findFile}
                     />
                 ))}
+                <button onClick={() => dispatch(addNewBlock(allBlocks.length))}>
+                    +
+                </button>
             </ul>
             {children[findFile]}
         </div>
